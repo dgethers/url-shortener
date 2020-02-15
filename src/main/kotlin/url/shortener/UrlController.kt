8ba453d69341e737b9cb9c703cb1fc9b3312fe2f
David Gethers <dgethers.jr@gmail.com>
2020-02-15 18:11:11 -0500

    Add ability to add url mappings

--- src/main/kotlin/url/shortener/UrlController.kt
+++ src/main/kotlin/url/shortener/UrlController.kt
@@ -1,12 +1,12 @@
 package url.shortener
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.MutableHttpResponse
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.*
 import java.net.URI
 import java.net.URL
+import kotlin.random.Random
 
 @Controller("/")
 class UrlController {
@@ -21,10 +21,32 @@ class UrlController {
         }
     }
 
-     companion object UrlMap {
-        val urlMapping = mapOf(
+    @Post
+    @Status(HttpStatus.CREATED)
+    fun addUrlMapping(@Body urlMap: url.shortener.UrlMap): url.shortener.UrlMap {
+        val id = generateSemiUniqueId()
+        val entry = UrlMap(id, urlMap.url)
+        urlMapping[id] = entry.url
+
+        return entry
+    }
+
+    companion object UrlMap {
+        val urlMapping = mutableMapOf(
                 "ab3950a" to "https://democracynow.org",
                 "dm40s9m" to "http://mangaowl.com/"
         )
     }
+}
+
+data class UrlMap(val id: String, val url: String)
+
+fun generateSemiUniqueId(): String {
+    val charPool: List<Char> = ('a'..'z') + ('0'..'9')
+    val randomString = (1..8)
+            .map { _ -> Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("");
+
+    return randomString
 }
